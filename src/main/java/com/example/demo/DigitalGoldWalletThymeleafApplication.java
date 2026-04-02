8a5795996822a25e430fa0faaf847ab94f2cb99e
Balraj-o6 <balrajkv666@gmail.com>
2026-04-02 11:27:04 +0530

    Initial commit — project setup with Thymeleaf, Spring Web, WebClient

---
+++ src/main/java/com/example/demo/DigitalGoldWalletThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalGoldWalletThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalGoldWalletThymeleafApplication.class, args);
+	}
+
+}
