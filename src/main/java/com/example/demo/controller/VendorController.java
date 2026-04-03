e1fd16c122dbfac6b102b07cf71d0be9cb2000ba
AnantSingh555 <139130113+AnantSingh555@users.noreply.github.com>
2026-04-03 23:17:15 +0530

    VendorAPI page is ready. (#10)
    Co-authored-by: Anant <as9446947@gmail.com>

--- src/main/java/com/example/demo/controller/VendorController.java
+++ src/main/java/com/example/demo/controller/VendorController.java
@@ -2,7 +2,9 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.AdminVendorDTO;
 
+
 import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,10 +20,11 @@ public class VendorController {
 
     private final WebClient webClient;
 
-    public VendorController(@Value("${backend.baseUrl:http://localhost:8085}") String backendBaseUrl) {
-		this.webClient = WebClient.builder().baseUrl(backendBaseUrl).build();
-	}
-    
+    public VendorController(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder
+                .baseUrl("http://localhost:8085")
+                .build();
+    }
 
     @GetMapping("/vendors/by-quantity")
     public String getVendorsByQuantity(
